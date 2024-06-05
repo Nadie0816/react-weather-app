09b55d21b41cfbc7713ade12a97daf79e52cb79e
Nadie0816 <64112529+Nadie0816@users.noreply.github.com>
2024-06-05 10:11:43 -0400

    added github url

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        {" "}
+        This project was coded by Briana A and is{" "}
+        <a
+          href="https://github.com/Nadie0816/react-weather-app.git"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
