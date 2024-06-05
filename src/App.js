63b4015688ef06fd159031a2987d01aa10d61e6d
Nadie0816 <64112529+Nadie0816@users.noreply.github.com>
2024-06-05 10:38:32 -0400

    added boostrap

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        {" "}
-        This project was coded by Briana A and is{" "}
-        <a
-          href="https://github.com/Nadie0816/react-weather-app.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          {" "}
+          This project was coded by Briana A and is{" "}
+          <a
+            href="https://github.com/Nadie0816/react-weather-app.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
